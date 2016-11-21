0c61de14ec90c6708cca21ee97863e03e6babbde
rosenralev <rossralev@gmail.com>
2016-11-21 08:27:15 +0100

    data set added + frontend

--- frontend/src/survivor/survivor.js
+++ frontend/src/survivor/survivor.js
@@ -1,12 +1,25 @@
 'use strict'
 
-angular.module('survivor', [])
- .config(function($routeProvider){
+angular.module('survivor', ['ngTable'])
+    .config(function($routeProvider){
       $routeProvider.when("/survivor", {
-          templateUrl: "/views/survivor/index.html",
-          controller: "survivor.controller"
+          templateUrl: "./views/survivor/index.html",
+          controller: "survivor-controller"
       })
-  })
-  .controller("survivor.controller", ["$scope", function($scope){
-      $scope.a = 5;
-  }])
+    })
+    .controller("survivor-controller", ["$scope", function($scope){
+        $scope.statistic = [
+            {
+                id: 1,
+                pclass: 1,
+                sex: 'female',
+                age: '0-1',
+                survivors: '10',
+                victims: '0',
+                total: '10',
+                percentage: '100'
+            }
+        ];
+
+        self.tableParams = new NgTableParams({}, { dataset: $scope.statistic});
+    }])
